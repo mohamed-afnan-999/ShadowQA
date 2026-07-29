c304b73b5c703cb8115dd39866d18cf2a0264939
mohamed-afnan-999 <infinityaffu@gmail.com>
2026-07-29 11:07:29 +0530

    exposing only 2 MCP tools now

--- server/mcpServer.js
+++ server/mcpServer.js
@@ -1,5 +1,4 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';          // to actually instantiate an MCP server
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";     // transport channel between MCP client and server
 
 // instantiate MCP server object
 export const mcpServer = new McpServer({
@@ -8,31 +7,15 @@ export const mcpServer = new McpServer({
 });
 
 // Import tools
-import { transcribeRecruiterAudioTool} from "./Tools/transcribe_recruiter_audio.js";
-import { isolateInterviewSegmentTool } from "./Tools/isolate_interview_segment.js";
-import { runComplianceAuditTool } from "./Tools/run_compliance_audit.js";
-import {fetchHistoricalAuditTool} from "./Tools/fetch_historical_audit.js";
+import { runFullAudioAuditTool } from "./Tools/run_full_audio_audit.js";
+import { fetchHistoricalAuditTool } from "./Tools/fetch_historical_audit.js";
 
 // Register tools
 mcpServer.tool(
-    transcribeRecruiterAudioTool.name,
-    transcribeRecruiterAudioTool.description,
-    transcribeRecruiterAudioTool.schema,
-    transcribeRecruiterAudioTool.handler
-)
-
-mcpServer.tool(
-    isolateInterviewSegmentTool.name,
-    isolateInterviewSegmentTool.description,
-    isolateInterviewSegmentTool.schema,
-    isolateInterviewSegmentTool.handler
-)
-
-mcpServer.tool(
-    runComplianceAuditTool.name,
-    runComplianceAuditTool.description,
-    runComplianceAuditTool.schema,
-    runComplianceAuditTool.handler
+    runFullAudioAuditTool.name,
+    runFullAudioAuditTool.description,
+    runFullAudioAuditTool.schema,
+    runFullAudioAuditTool.handler
 )
 
 mcpServer.tool(
