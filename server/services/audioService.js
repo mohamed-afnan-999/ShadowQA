cced9fd1aed58fd82449ac89a2b61278719656a1
mohamed-afnan-999 <infinityaffu@gmail.com>
2026-08-03 12:58:35 +0530

    Add: Dynamic recruiter name fetch for higher accuracy transcription

--- server/services/audioService.js
+++ server/services/audioService.js
@@ -1,10 +1,9 @@
 import fs from "fs";
 import path from "path";
-import os from "os";
-import axios from 'axios';
 import ffmpeg from "fluent-ffmpeg";
 import { randomUUID } from "crypto";    // needed for temp mp3 filename
 import Groq from 'groq-sdk';
+import {connectToDatabase, getActiveRecruiters} from "./dbService.js";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
@@ -48,12 +47,24 @@ export const transcribeAudio = async (audioFilePath) => {
 
         const audioFile = fs.createReadStream(audioFilePath);
 
+        // === Dynamic Recruiter Name Extraction and Feed ====
+        // connect to DB and extract the recruiter names live (as it maybe updated)
+        const dbConnection = await connectToDatabase();
+        const recruiterNames = await getActiveRecruiters(dbConnection);
+        let recruiterNamesString = null;
+
+        if (!recruiterNames || recruiterNames.length === 0)
+            recruiterNamesString = "Hajira Arfain, Ayesha, Roobi Naz";
+        else {
+            recruiterNamesString = recruiterNames.map(r => r.name).join(',');
+        }
+
         // call the groq API for transcription
         let transcription = await groq.audio.transcriptions.create({
             file: audioFile,
             model: 'whisper-large-v3-turbo',
-            // this prompt gives whisper some vocabulary context to expect in the audio files
-            prompt: `Interview transcript for 'BiziBees'. Recruitment, BPO, customer support, human resources, candidate, salary, notice period, shift, voice process. Recruiter names include - Hajira, Ayesha`,
+            // TODO: Add recruiter names, company name (BiziBees outsourcing) - this prompt gives whisper some vocabulary context to expect in the audio files for better transcription
+            prompt: `Ensure that the recruiter names the company 'Bizi Bees Outsourcing'. This is an Interview clip for candidates being interviewed by recruiters from the comapny called 'Bizi Bees Outsourcing'. Recruitment, BPO, customer support, human resources, candidate, salary, notice period, shift, voice process. Recruiter names include - ${recruiterNamesString}`,
             response_format: 'verbose_json',     // for word-level timestamps
             timestamp_granularities: ['segment', 'word']
         });
